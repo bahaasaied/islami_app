39d3192aa6e320c2802012a21287dff6a77585e4
Bahaa <bahaasaied15@gmail.com>
2024-07-26 19:38:37 +0300

    Sebha tab

--- lib/taps/sebha_tab.dart
+++ lib/taps/sebha_tab.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 
-class SebhaTab extends StatelessWidget {
+class SebhaTab extends StatefulWidget {
   const SebhaTab({super.key});
+
+  @override
+  State<SebhaTab> createState() => _SebhaTabState();
+}
+
+class _SebhaTabState extends State<SebhaTab> {
+  int counter = 0;
+  double angle = 0;
+  List<String> Azkar = [
+    "سبحان الله",
+    "الحمد الله",
+    "لا اله الا الله",
+    "الله اكبر",
+  ];
+  int index = 0;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       mainAxisAlignment: MainAxisAlignment.start,
-
       children: [
         Stack(
           alignment: Alignment.topCenter,
@@ -18,18 +32,70 @@ class SebhaTab extends StatelessWidget {
               child: Image.asset(
                 'assets/images/head_sebha_logo.png',
                 height: 105,
-
               ),
-            ),Padding(
+            ),
+            Padding(
               padding: const EdgeInsets.only(top: 80.0),
-              child: Image.asset(
-                'assets/images/body_sebha_logo.png',
-                height: 234,
+              child: Transform.rotate(
+                angle: angle,
+                child: GestureDetector(
+                  onTap: () {
+                    OnTap();
+                  },
+                  child: Image.asset(
+                    'assets/images/body_sebha_logo.png',
+                    height: 234,
+                  ),
+                ),
               ),
             ),
           ],
         ),
+        Padding(
+          padding: const EdgeInsets.only(top: 27.0),
+          child: Text(
+            'عدد التسبيحات',
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 26.0),
+          child: Center(
+            child: Container(
+              padding: EdgeInsets.all(14),
+              decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                  borderRadius: BorderRadius.circular(30)),
+              child: Text('$counter'),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 26.0),
+          child: Center(
+            child: Container(
+              padding: EdgeInsets.all(14),
+              decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                  borderRadius: BorderRadius.circular(30)),
+              child: Text('${Azkar[index]}'),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 
-    ],);
+  OnTap() {
+    counter++;
+    if (counter % 33 == 0) {
+      index++;
+      counter=0;
+    }
+    if(index ==Azkar.length){
+      index=0;
+    }
+    angle += 360 / 4;
+    setState(() {});
   }
 }
